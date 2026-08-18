11f65135fc5e0312a1d0d53d055a82c4d72e6a00
Phil-Walker93 <pukfliege37@gmail.com>
2026-08-19 00:53:44 +0200

    Add v0.7.4 client sync marker

---
+++ releases/0.7.4/files/v074.js
@@ -0,0 +1,4 @@
+/* LS Connect v0.7.4 client sync hotfix */
+const LS_CONNECT_V074_VERSION = '0.7.4';
+if (typeof els !== 'undefined' && els.connectionLabel) els.connectionLabel.textContent = 'Roleplay Messenger - v0.7.4';
+console.info('[LS Connect] v0.7.4 client sync hotfix active');
